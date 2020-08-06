d49d5a3e50846bd9f011551bc7ee5ff366f01552
Chandni Nagda <chandninagda@gmail.com>
2020-08-06 09:47:38 -0700

    terms topics, top terms field, scores added to top terms and top docs #7

--- topics_to_mongo.R
+++ topics_to_mongo.R
@@ -11,6 +11,15 @@ dt_path <- "../tmp/doctopics-75.rds"
 ldavis_path <- "../tmp/ldavis-75.json"
 qid_path <- "../tmp/qid.rds"
 ntopics <- 75
+ntopfields <- 5  # number of top field values listed per topic
+ntopdocs <- 5  # number of top docs listed per topic
+beta <- 0.01  # topic model param
+
+
+# Names of n largest values in list
+topn <- function(x, n) {
+    names(head(sort(unlist(x), decreasing = TRUE), n))
+}
 
 
 # Get the top metadata fields for each topic
@@ -40,10 +49,46 @@ get_top_fields <- function(meta, doc_topics, colname) {
     
     res <- transpose(res)
     topn <- function(x, n) names(head(sort(unlist(x), decreasing = TRUE), n))
-    res <- lapply(res, topn, 5)
+    res <- lapply(res, topn, ntopfields)
     
-    sapply(res, toJSON)
+    # sapply(res, toJSON)
 }
+
+
+# Get top documents for topic
+get_top_docs <- function(doc_topics) {
+    qids <- doc_topics$QID
+    doc_topics$QID <- NULL
+    topic_docs <- t(doc_topics)
+    colnames(topic_docs) <- qids
+    
+    topn <- function(x, n) {
+        vec <- head(sort(unlist(x), decreasing = TRUE), n)
+        as_list <- function(i) list(qid = as.integer(names(vec)[i]), 
+                                    score = vec[i])
+        lapply(1:length(vec), as_list)
+    }
+    process_topic <- function(topic) {
+       topn(topic_docs[topic,], ntopdocs)
+    }
+    lapply(1:nrow(topic_docs), process_topic)
+}
+
+
+# Get top terms for topic
+get_top_terms <- function(topic_terms) {
+    topn <- function(x, n) {
+        vec <- head(sort(unlist(x), decreasing = TRUE), n)
+        as_list <- function(i) list(term = names(vec)[i], 
+                                    score = vec[i])
+        lapply(1:length(vec), as_list)
+    }
+    process_topic <- function(topic) {
+        topn(topic_terms[topic,], ntopdocs)
+    }
+    lapply(1:nrow(topic_terms), process_topic)
+}
+
 
 # read in metadata csv
 meta <- read.csv(meta_path, row.names = NULL, stringsAsFactors = FALSE)
@@ -52,15 +97,21 @@ meta <- meta[match(qid$File_ID, meta$File_ID),]
 meta$QID <- qid$QID
 
 # read in doc topics
-doc_topics <- as.data.frame(readRDS(dt_path))
+theta <- t(readRDS(dt_path))
+alpha <- 5 / nrow(theta) # mallet default is 5/K
+theta <- theta + alpha # smooth
+doc_topics <- t(theta / rowSums(theta)) # normalize
 get_id <- function(x) sub("^([^.]*).*", "\\1", basename(x))
+doc_topics <- as.data.frame(doc_topics)
 doc_topics$File_ID <- sapply(rownames(doc_topics), get_id)
 doc_topics <- doc_topics[match(qid$File_ID, doc_topics$File_ID),]
 doc_topics$QID <- qid$QID
 doc_topics$File_ID <- NULL
 
 # read in topic terms
-topic_terms <- as.data.frame(t(readRDS(tt_path)))
+phi <- as.data.frame(readRDS(tt_path))
+phi <- phi + beta # smooth
+topic_terms <- as.data.frame(phi / rowSums(phi))
 
 # read in ldavis
 ldavis <- rjson::fromJSON(file = ldavis_path)
@@ -79,15 +130,23 @@ topics$locations <- get_top_fields(meta, doc_topics, "Location")
 topics$keywords <- get_top_fields(meta, doc_topics, "Keywords")
 topics$publishers <- get_top_fields(meta, doc_topics, "Publisher")
 
+# add top docs and terms
+topics$topDocs <- get_top_docs(doc_topics)
+topics$topTerms <- get_top_terms(topic_terms)
+
 # add proportions
 topic_freq <- colSums(doc_topics[,1:ntopics], na.rm = TRUE)
 topics$proportion <- topic_freq / sum(topic_freq)
 
-# add proportions
+# json
+topic_list <- transpose(topics)
+docs <- sapply(topic_list, toJSON, auto_unbox = TRUE)
+
+
 df <- data.frame(topics)
 colnames(df)[1] <- "_id"
 
 # write to db
 m <- mongo("topics", url = mongo_url)
 m$remove('{}')
-m$insert(df, rownames = FALSE)
+m$insert(docs, rownames = FALSE)
--- tt_to_mongo.R
+++ tt_to_mongo.R
@@ -7,30 +7,30 @@ tt_path <- "../tmp/topicterms-75.rds"
 beta <- 0.01  # topic model param
 
 
-# topic terms matrix
+# terms topics matrix
 phi <- as.data.frame(readRDS(tt_path))
 phi <- phi + beta # smooth
-df <- phi / rowSums(phi) # normalize
-df$topic_num <- seq.int(nrow(df))
+df <- t(phi / rowSums(phi))
+
 
 # initialize a mongodb connection and remove existing docs in collection
-m <- mongo("topics.terms", url = mongo_url)
+m <- mongo("terms.topics", url = mongo_url)
 m$remove('{}')
 
 # convert dataframe rows to json strings
-row_to_json <- function(row) {
-    freqs <- row[2:length(row) - 1]
-    to_list <- function(x) list(term = unbox(x), 
-                                probability = unbox(freqs[[x]]))
-    terms <- lapply(names(freqs), to_list)
+row_to_json <- function(i) {
+    row <- df[i,]
+    to_list <- function(j) list(topic = unbox(j), 
+                                probability = unbox(df[i,j]))
+    topics <- lapply(1:ncol(df), to_list)
     
     data <- list(
-        `_id` = unbox(unlist(row["topic_num"])),
-        terms = terms
+        `_id` = unbox(rownames(df)[i]),
+        topics = topics
     )
     toJSON(data, digits = NA)
 }
-docs <- apply(df, 1, row_to_json)
+docs <- sapply(1:nrow(df), row_to_json)
 
 # insert all documents
 m$insert(docs)
