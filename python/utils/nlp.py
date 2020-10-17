3c3547774146384b094365da228fc3c352f19b5a
Arthur Koehl <avkoehl@ucdavis.edu>
2020-10-17 00:44:53 -0700

    more punct and cleaned up

--- python/utils/nlp.py
+++ python/utils/nlp.py
@@ -1,4 +1,3 @@
-import string
 import re
 
 import nltk
@@ -9,7 +8,7 @@ def normalize(text):
     """
     Given string of words (whitespace delim), return string of normalized words
     """
-    PUNCT_RE = r'[!"#$%&\'()*+,./:;<=>?@\^_`{|}~]'
+    PUNCT_RE = r'[\[\]|!"#$%&\'()*+,./:;<=>?@\^_`{|}~]'
     s = stopwords.words('english')
     cleaned = []
 
@@ -17,7 +16,7 @@ def normalize(text):
     text = re.sub(r'\d+', '', text)
     text = re.sub(PUNCT_RE, '', text)
     text = re.sub(r'\s\s+', ' ', text)  # Handle excess whitespace
-    text = ' '.join(x for x in text.split() if x not in s)
+    text = ' '.join(x for x in text.split() if x not in s and len(x) > 1)
     text = text.strip()  # No whitespace at start and end of string
 
     return text
