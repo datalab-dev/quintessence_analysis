91e09042cd8d57fc5f03376ba019ecebe6cfe55f
Arthur Koehl <avkoehl@ucdavis.edu>
2020-10-16 23:44:19 -0700

    gets data

--- python/main.py
+++ python/main.py
@@ -1,4 +1,8 @@
 from models.topicmodel import TopicModel
+from utils.mongo import Mongo
 
+mongo = Mongo("../mongo_credentials.json")
+lda = TopicModel("../data/topicmodel/")
 
-lda = TopicModel("../data/")
+lemma = mongo.get_topic_model_data()
+
--- python/utils/mongo.py
+++ python/utils/mongo.py
@@ -18,43 +18,42 @@ class Mongo:
         self.db = client[credentials['database']]
 
     def get_topic_model_data(self):
-        cursor = db.docs.find(projection={'lemma': 1}).sort('_id', 1)
-        docs = cursor[:]
+        docs = list(self.db["docs.lemma"].find({}))
         return docs
 
-    def write_topic_model_data(model):
-        doctopics = model.load_document_topics()
-        phi = self.model.load_word_topics()
-        beta = 0.01 # mallet default
-        phi = phi + beta
-
-        # Create docs.topics
-        docs = []
-        for qid, row in enumerate(doc_topics):
-            topics = []
-            for topic, probability in row:
-                topics.append({'topicId': topic, 'probability': probability})
-            docs.append({'_id': qid, 'topics': topics})
-
-        db['docs.topics'].remove({})
-        db['docs.topics'].insert_many(docs)
-
-       # Create terms.topics
-        termtopics = np.apply_along_axis(lambda x: x / x.sum(), 0, phi) # normalize and smooth document topics
-        topicterms = np.transpose(termtopics)
-
-        docs = []
-        for i in range(topicterms.shape[1]):
-            term = self.mode.id2word[i]
-            topics = [{'topicId': j+1, 'probability': p} for j, p in
-                      enumerate(topicterms[term])]
-            docs.append({'_id': term, 'topics': topics})
-
-        db['terms.topics'].remove({})
-        db['terms.topics'].insert_many(docs)
-
-        """
-        Create topics
-        """
-        # TODO everything (see topics_to_mongo.R)
-        pass
+#    def write_topic_model_data(model):
+#        doctopics = model.load_document_topics()
+#        phi = self.model.load_word_topics()
+#        beta = 0.01 # mallet default
+#        phi = phi + beta
+#
+#        # Create docs.topics
+#        docs = []
+#        for qid, row in enumerate(doc_topics):
+#            topics = []
+#            for topic, probability in row:
+#                topics.append({'topicId': topic, 'probability': probability})
+#            docs.append({'_id': qid, 'topics': topics})
+#
+#        db['docs.topics'].remove({})
+#        db['docs.topics'].insert_many(docs)
+#
+#       # Create terms.topics
+#        termtopics = np.apply_along_axis(lambda x: x / x.sum(), 0, phi) # normalize and smooth document topics
+#        topicterms = np.transpose(termtopics)
+#
+#        docs = []
+#        for i in range(topicterms.shape[1]):
+#            term = self.mode.id2word[i]
+#            topics = [{'topicId': j+1, 'probability': p} for j, p in
+#                      enumerate(topicterms[term])]
+#            docs.append({'_id': term, 'topics': topics})
+#
+#        db['terms.topics'].remove({})
+#        db['terms.topics'].insert_many(docs)
+#
+#        """
+#        Create topics
+#        """
+#        # TODO everything (see topics_to_mongo.R)
+#        pass
