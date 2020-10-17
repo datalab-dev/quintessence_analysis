0bc0b9f2e08c17b5c9cec9bab3fc41a80794af22
Arthur Koehl <avkoehl@ucdavis.edu>
2020-10-17 16:01:50 -0700

    trains

--- python/main.py
+++ python/main.py
@@ -2,9 +2,15 @@ from models.topicmodel import TopicModel
 from utils.mongo import Mongo
 from utils.nlp import normalize
 
-mongo = Mongo("../mongo_credentials.json")
-lda = TopicModel("../data/topicmodel/")
+from joblib import Parallel, delayed
 
-lemma = mongo.get_topic_model_data()
-t = [normalize(doc) for doc in lemma]
+mongo = Mongo("../mongo_credentials.json")
+lda = TopicModel(model_path = "../data/topicmodel/", 
+        mallet_path= "/usr/local/bin/mallet",
+        num_topics = 60)
+
+docs = mongo.get_topic_model_data()
+docs = Parallel(n_jobs=4)(delayed(normalize)(doc) for doc in docs)
+
+lda.train(docs)
 
