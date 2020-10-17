4ad614a2f4bbaf9bb8d80bb5fd98cbb08d61c472
Arthur Koehl <avkoehl@ucdavis.edu>
2020-10-17 01:06:02 -0700

    reflect changed name of function

--- python/main.py
+++ python/main.py
@@ -1,10 +1,10 @@
 from models.topicmodel import TopicModel
 from utils.mongo import Mongo
-from utils.nlp import tokenize
+from utils.nlp import normalize
 
 mongo = Mongo("../mongo_credentials.json")
 lda = TopicModel("../data/topicmodel/")
 
 lemma = mongo.get_topic_model_data()
-t = [preprocess(doc) for doc in lemma[0:3]]
+t = [normalize(doc) for doc in lemma]
 
