b82bddf1cd0c66157ba3b5576f40f6fee2d94f00
Arthur Koehl <avkoehl@ucdavis.edu>
2020-10-17 16:10:17 -0700

    structure

--- python/main.py
+++ python/main.py
@@ -4,13 +4,14 @@ from utils.nlp import normalize
 
 from joblib import Parallel, delayed
 
+print("initializing classes")
 mongo = Mongo("../mongo_credentials.json")
 lda = TopicModel(model_path = "../data/topicmodel/", 
         mallet_path= "/usr/local/bin/mallet",
         num_topics = 60)
 
+print("running topic model")
 docs = mongo.get_topic_model_data()
+docs = docs[0:20]
 docs = Parallel(n_jobs=4)(delayed(normalize)(doc) for doc in docs)
-
 lda.train(docs)
-
