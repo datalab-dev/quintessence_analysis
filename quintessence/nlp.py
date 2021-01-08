fe8f2420906e8727a338129fffc5ac0ed24dbc53
Arthur Koehl <avkoehl@ucdavis.edu>
2021-01-07 17:01:39 -0800

    not sure why this was wrong, but fixed

--- quintessence/nlp.py
+++ quintessence/nlp.py
@@ -30,7 +30,7 @@ def normalize_text(text,
 def compute_proportions(doctopics, doc_lens):
     """ Compute corpus wide topic proportions """
     weighted = np.multiply(doctopics.todense(), doc_lens)
-    return np.sum(weighted, axis = 1) / np.sum(weighted).A
+    return np.sum(weighted, axis = 1) / np.sum(weighted)
 
 def compute_coordinates(topicterms):
     """ Compute x and y coordinates for topics using multidimensional scaling of topic terms matrix """
@@ -57,6 +57,7 @@ def get_subsets (metacol):
 
 def subset_word_counts(subsets):
     pass
+
 def subset_proportions(subsets):
     """  for each metadata grouping, compute the mean nonzero topic proportion 
     This is slightly different then the compute proportions function becuase it doesn't take into account the document lengths, treating each document as equal
