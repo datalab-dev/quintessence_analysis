6c41d50e1dee03e829c8ab30153a146c5e31f8b9
Arthur Koehl <avkoehl@ucdavis.edu>
2021-01-14 23:03:48 -0800

    move sentences out of mongo connection and fix return order of ids and docs

--- quintessence/mongo.py
+++ quintessence/mongo.py
@@ -4,11 +4,13 @@ import pandas as pd
 import numpy as np
 from pymongo import MongoClient
 from gensim.models.wrappers import LdaMallet
+from joblib import Parallel
+from joblib import delayed
 
-from quintessence.topicmodel import TopicModel
 from quintessence.parse_lda import create_doc_topics
 from quintessence.parse_lda import create_topic_terms
 from quintessence.parse_lda import create_topics
+from quintessence.topicmodel import TopicModel
 
 class Mongo:
     def __init__(self, credentials_path):
@@ -36,9 +38,7 @@ class Mongo:
         docs = [" ".join(r["std"].split('\t')) for r in res]
         ids = [r["_id"] for r in res]
 
-        sentences = Parallel(n_jobs=4)(
-                delayed(tokenize)(d for d in docs))
-        return sentences
+        return ids,docs
 
     def get_topic_model_data(self):
         """ return ids, and strings of lemmatized documents """
