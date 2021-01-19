25a2019c96e795ac6f14d4b3707306ad0c4beb53
Arthur Koehl <avkoehl@ucdavis.edu>
2021-01-19 15:05:58 -0800

    instead of two lists, save as series

--- quintessence/corpus.py
+++ quintessence/corpus.py
@@ -3,21 +3,26 @@ An object to hold the metadata and text documents (in several forms)
 as they are pulled from the database, preprocessed and sent to the models 
 Plan is to create one for the topic model data and one for the word2vec data.
 """
+import pandas as pd
+
 from quintessence.nlp import normalize_text
 from quintessence.nlp import sentence_tokenize
 
 class Corpus:
-    def __init__(self, meta, doclist, ids=range(0,len(doclist))):
-        self.meta = meta
-        self.docs = doclist
-        self.ids = ids
-        self.sentences = []
-        self.doc_sentences = []
+    sentences = []
+    doc_sentences = []
 
-    def topic_model_preprocessing():
-        self.docs = [normalize_text(d) for d in self.docs]
+    def __init__(self, meta, docs):
+        self.meta = meta # pd dataframe
+        self.docs = docs # pd Series
 
-    def embed_preprocessing():
-        self.doc_sentences = [sentence_tokenzie(d) for d in docs]
+    def topic_model_preprocessing(self):
+        # TODO: parallelize
+        normalized = [normalize_text(d) for d in self.docs]
+        self.docs = pd.Series(normalized, index=self.docs.index)
+
+    def embed_preprocessing(self):
+        # TODO: parallelize
+        self.doc_sentences = [sentence_tokenize(d) for d in docs]
         self.sentences = [normalize_text(s).split() for sents in doc_sentences
                 for s in sents]
