c83d635f8a19d0e0a6d01968f8888c503eb9a5eb
Arthur Koehl <avkoehl@ucdavis.edu>
2021-01-19 16:04:55 -0800

    working on embeddings now

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-""" Main script that runs LDA, word2vec models and updates the database
+""" 
+Main script that runs LDA, word2vec models and updates the database
 
-    usage: poetry run python main.py
+usage: poetry run python main.py
 """
-from joblib import Parallel, delayed
-
+from quintessence.corpus import Corpus
 from quintessence.mongo import Mongo
 from quintessence.topicmodel import TopicModel
-from quintessence.nlp import normalize_text
+from quintessence.embeddings import Embeddings
+
+def run_topic_model(con):
+    """ Given connection to database, run topic model """
+    corpus = Corpus(con.get_metadata(), con.get_topic_model_data())
+    corpus.topic_model_preprocessing()
+    lda = TopicModel("./data/topicmodel/", 
+             mallet_path = "/usr/local/bin/mallet",
+             num_topics = 5)
+    lda.train(corpus.docs)
+    con.write_topic_model_data(lda)
+    return
 
 # Connect to Database
 print("Connecting to Database")
 con = Mongo("./mongo_credentials.json")
 
-# Run Topic Model + Write to DB
-print("Running Topic Model")
-ids, docs = con.get_topic_model_data()
-docs = [normalize_text(doc) for doc in docs]
-lda = TopicModel("./data/topicmodel/", 
-        mallet_path = "/usr/local/bin/mallet",
-        num_topics = 10)
-lda.train(docs[0:10], ids[0:10])
-# write to db
-# free model
+# Topic Model
+# run_topic_model(con)
 
 # Run Word Embedding + Write to DB
-# print("Run Word Embeddings")
-
-
+corpus = Corpus(con.get_metadata(), con.get_embeddings_data())
+corpus.embed_preprocessing()
+embed = Embeddings("./data/embeddings/")
+embed.train(corpus.sentences)
+#con.write_embeddings_data(embed)
