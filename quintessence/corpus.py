37893e0a661e3979982fd1d486bc1e5e4e4ba65f
Arthur Koehl <avkoehl@ucdavis.edu>
2021-01-21 17:04:30 -0800

    progress

--- quintessence/corpus.py
+++ quintessence/corpus.py
@@ -7,10 +7,13 @@ import pandas as pd
 
 from quintessence.nlp import normalize_text
 from quintessence.nlp import sentence_tokenize
+from quintessence.nlp import list_group_by
 
 class Corpus:
-    sentences = []
-    doc_sentences = []
+    meta = None
+    docs = None
+    sentences = None
+    doc_sentences = None
 
     def __init__(self, meta, docs):
         self.meta = meta # pd dataframe
@@ -21,7 +24,37 @@ class Corpus:
         self.docs = pd.Series(normalized, index=self.docs.index)
 
     def embed_preprocessing(self):
+        """ given meta and docs, return pandas dataframe subset sentence """
         self.doc_sentences = [sentence_tokenize(d) for d in self.docs]
+        self.doc_sentences = pd.Series(
+                self.doc_sentences, index=self.docs.index)
+
+        subsets = compute_embedding_subsets()
+
         self.sentences = [normalize_text(s).split() 
                 for sents in self.doc_sentences
                 for s in sents]
+
+    def compute_embedding_subsets(self):
+        """ given meta return, series qid, subset """
+        meta = self.meta
+        docs = self.docs
+        meta["wordcounts"] = [len(d) for d in docs]
+        meta["decade"] = [d[0:3] + "0" for d in meta["Date"]]
+
+        # decades, authors, locations
+        decades = meta.groupby("decade").sum("wordcounts")
+        locations = meta.groupby("Location").sum("wordcounts")
+        authors_inds = list_group_by(meta["Author"])
+        wcs = []
+        for k,v in authors_inds.items():
+            wcs.append(meta["wordcounts"][v].sum())
+        authors = pd.Series(wcs, index=authors_inds.keys())
+
+        # keep only those with wordocunt > 2 million
+        decades = list(decades[decades["wordcounts"] > 100000].index)
+        locations = list(locations[locations["wordcounts"] > 100000].index)
+        authors = list(authors[authors > 100000].index)
+
+        pass
+
