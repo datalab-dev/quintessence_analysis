9449b08ae326a0530bde4a568e00ba6ee0fda8ac
Arthur Koehl <avkoehl@ucdavis.edu>
2021-02-08 23:04:25 -0800

    all coming together

--- main.py
+++ main.py
@@ -3,32 +3,19 @@ Main script that runs LDA, word2vec models and updates the database
 
 usage: poetry run python main.py
 """
-from quintessence.corpus import Corpus
-from quintessence.mongo import Mongo
-from quintessence.topicmodel import TopicModel
-from quintessence.embeddings import Embeddings
+import json
 
-def run_topic_model(con):
-    """ Given connection to database, run topic model """
-    corpus = Corpus(con.get_metadata(), con.get_topic_model_data())
-    corpus.topic_model_preprocessing()
-    lda = TopicModel("./data/topicmodel/", 
-             mallet_path = "/usr/local/bin/mallet",
-             num_topics = 5)
-    lda.train(corpus.docs)
-    con.write_topic_model_data(lda)
-    return
+from quintessence.pipeline import EmbeddingsPipeline
+from quintessence.pipeline import TopicModelPipeline
 
-# Connect to Database
-print("Connecting to Database")
-con = Mongo("./mongo_credentials.json")
+## load in json config file
+args = json.load(open("config.json"))
 
-# Topic Model
-# run_topic_model(con)
+## LDA
+TopicModelPipeline(args)
 
-# Run Word Embedding + Write to DB
-corpus = Corpus(con.get_metadata(), con.get_embeddings_data())
-corpus.embed_preprocessing()
-embed = Embeddings("./data/embeddings/")
-embed.train(corpus.sentences)
-#con.write_embeddings_data(embed)
+## Embeddings
+EmbeddingsPipeline(args)
+
+
+
--- quintessence/pipeline.py
+++ quintessence/pipeline.py
@@ -1,39 +1,42 @@
-import json
-
 from quintessence.corpus import TMCorpus
 from quintessence.corpus import EmbedCorpus
 from quintessence.embeddings import Embeddings
 from quintessence.mongo import Mongo
 from quintessence.topicmodel import TopicModel
 
-args = json.load(open('config.json'))
-
 def TopicModelPipeline(args):
+    print("Training topic Model")
 
     # 1. get data
+    print("... preprocessing")
     con = Mongo(args["mongo_credentials"])
     corpus = TMCorpus(con.get_metadata(), 
             con.get_topic_model_data(),
             args["ncores"])
 
     # 2. train
+    print("... training")
     lda = TopicModel(args["topic_model"]["tmodir"])
     lda.train(corpus.docs, 
             args["topic_model"]["mallet_path"], 
             args["topic_model"]["num_topics"])
 
     # 3. save to database
+    print("... saving to DB")
     con.write_topic_model_data(lda)
 
 def EmbeddingsPipeline(args):
+    print("training word embeddings")
 
     # 1. get data
+    print("... preprocessing")
     con = Mongo(args["mongo_credentials"])
     corpus = EmbedCorpus(con.get_metadata(), 
             con.get_embeddings_data(),
             args["ncores"])
 
     # 2. train
+    print("... training")
     embed = Embeddings(args["embedding"]["embedodir"])
     embed.train_all(corpus.doc_sentences, 
             corpus.subsets,
@@ -43,4 +46,5 @@ def EmbeddingsPipeline(args):
             args["ncores"])
 
     # 3. save to database
+    print("... saving to DB")
     con.write_embeddings_data(embed)
