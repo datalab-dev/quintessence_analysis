34e6e6c192addcae341974ddbafd04a76fc8dee1
Arthur Koehl <avkoehl@ucdavis.edu>
2021-02-18 16:47:34 -0800

    easier to debug interactively this way than with the pipeline functions

--- main.py
+++ main.py
@@ -5,17 +5,41 @@ usage: poetry run python main.py
 """
 import json
 
-from quintessence.pipeline import EmbeddingsPipeline
-from quintessence.pipeline import TopicModelPipeline
+import numpy as np
+import pandas as pd
+
+from quintessence.embeddings import Embeddings
+from quintessence.mongo import Mongo
+from quintessence.topicmodel import TopicModel
 
 ## load in json config file
 args = json.load(open("config.json"))
 
-## LDA
-TopicModelPipeline(args)
+# TOPIC MODEL
+con = Mongo(args["mongo_credentials"])
+corpus = con.get_topic_model_data()
 
-## Embeddings
-EmbeddingsPipeline(args)
+lda = TopicModel(args["topic_model"]["tmodir"])
+lda.train(corpus,
+        args["topic_model"]["mallet_path"],
+        args["topic_model"]["num_topics"],
+        workers = args["ncores"])
+
+con.write_topic_model_data(corpus,lda)
 
 
+# EMBEDDING
+corpus = con.get_embeddings_data()
 
+# 2. train
+print("Run Word Embeddings")
+embed = Embeddings(args["embedding"]["embedodir"])
+embed.train_all(corpus,
+       sg = args["embedding"]["sg"],
+       window = args["embedding"]["window"],
+       size = args["embedding"]["size"],
+       workers = args["ncores"])
+
+# 3. save to database
+print("Saving to DB")
+con.write_embeddings_data(embed)
