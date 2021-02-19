34b12815e5021a0ea9b741c68e8b1b823ae02b1e
Arthur Koehl <avkoehl@ucdavis.edu>
2021-02-18 16:47:05 -0800

    update pipeline to reflect lack of corpus class

--- quintessence/pipeline.py
+++ quintessence/pipeline.py
@@ -1,5 +1,3 @@
-from quintessence.corpus import TMCorpus
-from quintessence.corpus import EmbedCorpus
 from quintessence.embeddings import Embeddings
 from quintessence.mongo import Mongo
 from quintessence.topicmodel import TopicModel
@@ -8,43 +6,39 @@ def TopicModelPipeline(args):
     print("Training topic Model")
 
     # 1. get data
-    print("... preprocessing")
+    print("Get data")
     con = Mongo(args["mongo_credentials"])
-    corpus = TMCorpus(con.get_metadata(), 
-            con.get_topic_model_data(),
-            args["ncores"])
+    corpus = con.get_topic_model_data()
 
     # 2. train
-    print("... training")
+    print("Run Topic Model")
     lda = TopicModel(args["topic_model"]["tmodir"])
-    lda.train(corpus.docs, 
+    lda.train(corpus,
             args["topic_model"]["mallet_path"], 
-            args["topic_model"]["num_topics"])
+            args["topic_model"]["num_topics"],
+            workers = args["ncores"])
 
     # 3. save to database
-    print("... saving to DB")
-    con.write_topic_model_data(lda)
+    print("Saving to DB")
+    con.write_topic_model_data(corpus, lda)
 
 def EmbeddingsPipeline(args):
     print("training word embeddings")
 
     # 1. get data
-    print("... preprocessing")
+    print("Get data")
     con = Mongo(args["mongo_credentials"])
-    corpus = EmbedCorpus(con.get_metadata(), 
-            con.get_embeddings_data(),
-            args["ncores"])
+    corpus = con.get_embeddings_data()
 
     # 2. train
-    print("... training")
+    print("Run Word Embeddings")
     embed = Embeddings(args["embedding"]["embedodir"])
-    embed.train_all(corpus.doc_sentences, 
-            corpus.subsets,
-            args["embedding"]["sg"],
-            args["embedding"]["window"],
-            args["embedding"]["size"],
-            args["ncores"])
+    embed.train_all(corpus,
+            sg = args["embedding"]["sg"],
+            window = args["embedding"]["window"],
+            size = args["embedding"]["size"],
+            workers = args["ncores"])
 
     # 3. save to database
-    print("... saving to DB")
+    print("Saving to DB")
     con.write_embeddings_data(embed)
